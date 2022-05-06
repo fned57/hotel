f46449809444c73b1aa9bb89d228fe4bcc59597d
Trần Đăng Tuấn (TRN.Fresher) <tuan.tran@bnksolution.com>
2022-05-07 01:01:09 +0700

    update roomtype and room

--- models/holel_room_type.py
+++ models/holel_room_type.py
@@ -7,7 +7,8 @@ class HotelRoomType(models.Model):
 
     name = fields.Char(string="Room type")
     description = fields.Text("Description room type")
-    price = fields.Float(string="Price Room")
-    price_hour = fields.Float(string="Price Hour")
-    price_overnight = fields.Float(string="Price over night")
+    price = fields.Float(string="Price Room", digits=(12, 0))
+    price_hour = fields.Float(string="Price Hour", digits=(12, 0))
+    price_overnight = fields.Float(string="Price over night", digits=(12, 0))
     room_ids = fields.One2many('hotel.room', 'room_type_id', string="Rooms")
+    currency_id = fields.Many2one('res.currency', 'currency')
--- models/hotel_room.py
+++ models/hotel_room.py
@@ -1,9 +1,11 @@
+from email.policy import default
+
 from odoo import  fields, models, api
 
 
 class HotelRoom(models.Model):
     _name = 'hotel.room'
-    _inherit = 'mail.thread'
+    _inherit = ['mail.thread','mail.activity.mixin']
     _description = "Hotel Rooms"
 
     name = fields.Char(string="Name room")
@@ -13,7 +15,12 @@ class HotelRoom(models.Model):
     room_type_id = fields.Many2one('hotel.room.type', string="Room type")
     reservation_form_ids = fields.Many2many('hotel.reservation.form')
     evaluate_ids = fields.One2many('mail.message', string="Messages", store=False)
-    price = fields.Float(string="Price", related='room_type_id.price')
+    price = fields.Float(string="Price Room", digits=(12, 0), related='room_type_id.price')
+    price_hour = fields.Float(string="Price Hour", digits=(12, 0), related='room_type_id.price_hour')
+    price_overnight = fields.Float(string="Price over night", digits=(12, 0), related='room_type_id.price_overnight')
+    currency_id = fields.Many2one('res.currency', 'currency', compute='_compute_currency')
 
+    def _compute_currency(self):
+        self.currency_id = self.env.company.currency_id
 
 
