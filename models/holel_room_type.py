0f7487cf465e39fb485d87504d46a0da5dd7f9f6
Trần Đăng Tuấn (TRN.Fresher) <tuan.tran@bnksolution.com>
2022-05-09 00:20:50 +0700

    test1

--- models/holel_room_type.py
+++ models/holel_room_type.py
@@ -12,3 +12,7 @@ class HotelRoomType(models.Model):
     price_overnight = fields.Float(string="Price over night", digits=(12, 0))
     room_ids = fields.One2many('hotel.room', 'room_type_id', string="Rooms")
     currency_id = fields.Many2one('res.currency', 'currency')
+
+    def test(self):
+        print(1)
+        print(2)
